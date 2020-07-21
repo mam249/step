db50bc498b4d5e32331a5cabbace75b700e6fcab
Mohamed <mohamed.malem0@gmail.com>
2020-07-21 09:48:44 +0000

    implement a comments box and display name feature with anonymous ticker option

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -28,24 +28,36 @@ import java.util.ArrayList;
 
 /** Servlet that returns some example content. TODO: modify this file to handle comments data */
 @WebServlet("/data")
-public class DataServlet extends HttpServlet {
-  private ArrayList<String> trial_array;
-  private String json_array;
+public final class DataServlet extends HttpServlet {
 
   @Override
-  public void init() {
-    trial_array = new ArrayList<>();
-    trial_array.add("First hard-coded message");
-    trial_array.add("Second hard-coded message");
-    trial_array.add("Third hard-coded message");
-    json_array = new Gson().toJson(trial_array);
-  }
-  @Override
-  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+      // Get the input from the form.
+    String comment = getParameter(request, "comment-box", "");
+    String displayName = getParameter(request, "display-name", "");
+    boolean anonymous = Boolean.parseBoolean(getParameter(request, "anonymous", "false"));
+
+    if (anonymous) {
+        displayName = "Anonymous";
+    }
+
+    if (displayName == ""){
+        displayName = "Anonymous";
+    }
+
+    // Respond with the result.
     response.setContentType("text/html;");
-    response.getWriter().println("Hello Mohamed!");
-    response.setContentType("application/json;");
-    response.getWriter().println(json_array);
+    response.getWriter().println(comment + " - " + displayName);
+
+  }
+  
+
+  private String getParameter(HttpServletRequest request, String name, String defaultValue) {
+    String value = request.getParameter(name);
+    if (value == null) {
+      return defaultValue;
+    }
+    return value;
   }
 }
 
